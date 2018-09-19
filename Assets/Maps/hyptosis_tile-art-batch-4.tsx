891c3208490e49a9597fb2cb2c6c854f0b6aaf78
Marc Towler <marc@marctowler.co.uk>
2018-09-19 23:07:58 +0100

    Updated house interior to include a warp zone and black tiles to stop issues with the lack of skybox... Added blank character base class and a player class

---
+++ Assets/Maps/hyptosis_tile-art-batch-4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="hyptosis_tile-art-batch-4" tilewidth="16" tileheight="16" spacing="1" tilecount="3136" columns="56">
+ <image source="../../../../Desktop/Tilesets/hyptosis_tile-art-batch-4.png" width="960" height="960"/>
+</tileset>
